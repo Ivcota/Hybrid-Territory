2f34c16f1726416cd4063debcfc1398bc89b6544
Iverson Diles <ivcotad@gmail.com>
2022-06-07 15:50:51 -0700

    basic login and log out

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,11 +1,13 @@
 import { AuthProvider } from '@redwoodjs/auth'
 
+
 import { FatalErrorBoundary, RedwoodProvider } from '@redwoodjs/web'
 import { RedwoodApolloProvider } from '@redwoodjs/web/apollo'
 
 import FatalErrorPage from 'src/pages/FatalErrorPage'
 import Routes from 'src/Routes'
 
+import './scaffold.css'
 import './index.css'
 
 const App = () => (
--- web/src/Routes.tsx
+++ web/src/Routes.tsx
@@ -13,6 +13,10 @@ import BaseLayout from './layouts/BaseLayout/BaseLayout'
 const Routes = () => {
   return (
     <Router>
+      <Route path="/login" page={LoginPage} name="login" />
+      <Route path="/signup" page={SignupPage} name="signup" />
+      <Route path="/forgot-password" page={ForgotPasswordPage} name="forgotPassword" />
+      <Route path="/reset-password" page={ResetPasswordPage} name="resetPassword" />
       <Set wrap={BaseLayout}>
         <Route path="/about" page={AboutPage} name="about" />
         <Route path="/" page={HomePage} name="home" />
