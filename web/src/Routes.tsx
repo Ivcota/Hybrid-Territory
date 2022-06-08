3a4cc7f064f12e816a4dd9bfc5524c73e42675dc
Iverson Diles <ivcotad@gmail.com>
2022-06-08 08:51:20 -0700

    Update Routes.tsx

--- web/src/Routes.tsx
+++ web/src/Routes.tsx
@@ -7,19 +7,21 @@
 // 'src/pages/HomePage/HomePage.js'         -> HomePage
 // 'src/pages/Admin/BooksPage/BooksPage.js' -> AdminBooksPage
 
-import { Router, Route, Set } from '@redwoodjs/router'
+import { Router, Route, Set, routes, Private } from '@redwoodjs/router'
 import TerritoriesLayout from 'src/layouts/TerritoriesLayout'
 import BaseLayout from './layouts/BaseLayout/BaseLayout'
 
 const Routes = () => {
   return (
     <Router>
-      <Set wrap={TerritoriesLayout}>
-        <Route path="/admin/territories/new" page={TerritoryNewTerritoryPage} name="newTerritory" />
-        <Route path="/admin/territories/{id}/edit" page={TerritoryEditTerritoryPage} name="editTerritory" />
-        <Route path="/admin/territories/{id}" page={TerritoryTerritoryPage} name="territory" />
-        <Route path="/admin/territories" page={TerritoryTerritoriesPage} name="territories" />
-      </Set>
+      <Private unauthenticated="home" roles="admin">
+        <Set wrap={TerritoriesLayout}>
+          <Route path="/admin/territories/new" page={TerritoryNewTerritoryPage} name="newTerritory" />
+          <Route path="/admin/territories/{id}/edit" page={TerritoryEditTerritoryPage} name="editTerritory" />
+          <Route path="/admin/territories/{id}" page={TerritoryTerritoryPage} name="territory" />
+          <Route path="/admin/territories" page={TerritoryTerritoriesPage} name="territories" />
+        </Set>
+      </Private>
       <Set wrap={BaseLayout}>
         <Route prerender path="/about" page={AboutPage} name="about" />
         <Route prerender path="/" page={HomePage} name="home" />
