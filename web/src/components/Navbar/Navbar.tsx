cd84175b76756503c95e247e43801cbd9e999607
Iverson Diles <ivcotad@gmail.com>
2022-07-21 10:31:33 -0700

    context api + working on routes

--- web/src/components/Navbar/Navbar.tsx
+++ web/src/components/Navbar/Navbar.tsx
@@ -1,205 +1,35 @@
-import { useClickOutside } from '@mantine/hooks'
-import { useAuth } from '@redwoodjs/auth'
-import { Link, navigate, NavLink, routes } from '@redwoodjs/router'
-import { motion } from 'framer-motion'
-import { useEffect, useState } from 'react'
+import { Link, routes } from '@redwoodjs/router'
+import { useContext } from 'react'
 import { FiMenu, FiX } from 'react-icons/fi'
+import { layoutContext } from 'src/layouts/UserLayout/UserLayout'
 
-interface NavItem {
-  title: string
-  route: string
-  isAdmin?: boolean
+const Logo = () => {
+  return (
+    <div className="text-2xl">
+      <Link to={routes.home()}>
+        Hybrid<span className="font-bold">Territory</span>
+      </Link>
+    </div>
+  )
 }
 
 const Navbar = () => {
-  const [isOpen, setIsOpen] = useState(false)
-  const ref = useClickOutside(() => setIsOpen(false))
-  const { isAuthenticated, logOut, loading, currentUser } = useAuth()
-
-  const navLinkItems: NavItem[] = [
-    {
-      title: 'My Territories',
-      route: routes.myTerritories(),
-    },
-    {
-      title: 'Checkout Territory',
-      route: routes.selfCheckout(),
-    },
-    {
-      title: 'My Account',
-      route: routes.userAccount(),
-    },
-    {
-      title: 'Issue Tracking',
-      route: routes.issueTracker(),
-      isAdmin: true,
-    },
-    {
-      title: 'Assign Territory',
-      route: routes.assignTerritory(),
-      isAdmin: true,
-    },
-    {
-      title: 'Records',
-      route: routes.records(),
-      isAdmin: true,
-    },
-  ]
-
-  useEffect(() => {
-    if (!loading && !isAuthenticated) {
-      navigate(routes.home())
-    }
-  }, [isAuthenticated])
+  const { isActive, toggle, currentUser } = useContext(layoutContext)
 
   return (
-    <>
-      <header className="sticky top-0 z-20 bg-white shadow-md">
-        <nav className="flex justify-between px-2 py-4 mx-auto text-center md:max-w-4xl">
-          <div className="text-2xl">
-            <Link to={routes.home()}>
-              Hybrid<span className="font-bold">Territory</span>
-            </Link>
-          </div>
-          <ul className="hidden gap-4 md:flex">
-            {navLinkItems.map(({ title, route, isAdmin }, i) => {
-              if (isAdmin) {
-                return (
-                  <>
-                    {currentUser.roles === 'admin' && (
-                      <li key={i}>
-                        <NavLink
-                          to={route}
-                          activeClassName="underline text-orange-500 "
-                        >
-                          {title}
-                        </NavLink>
-                      </li>
-                    )}
-                  </>
-                )
-              }
-
-              return (
-                <li key={i}>
-                  <NavLink
-                    to={route}
-                    activeClassName="underline text-orange-500 "
-                  >
-                    {title}
-                  </NavLink>
-                </li>
-              )
-            })}
-
-            <li>
-              <button
-                onClick={() => {
-                  logOut()
-                }}
-              >
-                Logout
-              </button>
-            </li>
-          </ul>
-          <FiMenu
-            onClick={() => {
-              setIsOpen(true)
-            }}
-            className="md:hidden"
-            size={24}
-          />
-        </nav>
-      </header>
-
-      <motion.div
-        ref={ref}
-        initial={{
-          x: 400,
-        }}
-        variants={{
-          open: {
-            x: 0,
-          },
-          closed: {
-            x: 400,
-          },
-        }}
-        transition={{ duration: 0.4, ease: 'easeInOut' }}
-        animate={isOpen ? 'open' : 'closed'}
-        className="fixed z-20 min-h-screen shadow-lg top-0 right-0 bg-white w-[18rem]"
-      >
-        <FiX
-          className="absolute top-3 right-3"
-          size={20}
-          onClick={() => {
-            setIsOpen(false)
-          }}
-        />
-
-        <ul
-          onClick={() => {
-            setIsOpen(false)
-          }}
-          className="flex flex-col items-center min-h-screen gap-4 pt-10"
-        >
-          <li>
-            <NavLink
-              to={routes.myTerritories()}
-              activeClassName="underline text-orange-500 "
-            >
-              My Territories
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              to={routes.selfCheckout()}
-              activeClassName="underline text-orange-500 "
-            >
-              Checkout Territory
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              to={routes.userAccount()}
-              activeClassName="underline text-orange-500 "
-            >
-              My Account
-            </NavLink>
-          </li>
-          {currentUser?.roles === 'admin' && (
-            <li>
-              <NavLink
-                to={routes.issueTracker()}
-                activeClassName="underline text-orange-500 "
-              >
-                Issue Tracking
-              </NavLink>
-            </li>
+    <nav className="sticky top-0 py-4 bg-white shadow-lg ">
+      <div className="max-w-4xl px-3 mx-auto ">
+        <div className="flex items-center gap-5">
+          <Logo />
+          {!isActive ? (
+            <FiMenu onClick={toggle} size={25} />
+          ) : (
+            <FiX onClick={toggle} size={25} />
           )}
-
-          {currentUser?.roles === 'admin' && (
-            <li>
-              <NavLink
-                to={routes.assignTerritory()}
-                activeClassName="underline text-orange-500 "
-              >
-                Assign Territory
-              </NavLink>
-            </li>
-          )}
-          <li>
-            <button
-              onClick={() => {
-                logOut()
-              }}
-            >
-              Logout
-            </button>
-          </li>
-        </ul>
-      </motion.div>
-    </>
+        </div>
+        {isActive && <div className="mt-5">{currentUser.firstName}</div>}
+      </div>
+    </nav>
   )
 }
 
--- web/src/layouts/UserLayout/UserLayout.tsx
+++ web/src/layouts/UserLayout/UserLayout.tsx
@@ -1,16 +1,49 @@
+import { useAuth, CurrentUser } from '@redwoodjs/auth'
+import { NavLink, routes } from '@redwoodjs/router'
+import { createContext } from 'react'
 import Navbar from 'src/components/Navbar/Navbar'
+import { useToggle } from 'src/hooks/useToggle'
+
+interface LayoutContext {
+  isActive: boolean
+  toggle: () => void
+  currentUser?: CurrentUser
+}
+
+export const layoutContext = createContext<LayoutContext | null>(null)
 
 type UserLayoutProps = {
   children?: React.ReactNode
 }
 
 const UserLayout = ({ children }: UserLayoutProps) => {
+  const { currentUser } = useAuth()
+  const { isActive, toggle } = useToggle({ defaultActive: false })
+  const { Provider } = layoutContext
+
   return (
     <>
-      <Navbar />
-      <main>
-        <div className="max-w-4xl px-4 mx-auto mt-5">{children}</div>
-      </main>
+      <Provider
+        value={{
+          currentUser,
+          isActive,
+          toggle,
+        }}
+      >
+        <Navbar />
+
+        <main>
+          <div id="side-bar" className="sticky top-0 left-0 p-3 bg-red-300 ">
+            <NavLink to={routes.home()} activeClassName="text-blue-400">
+              Home
+            </NavLink>
+            <NavLink to={routes.home()} activeClassName="text-blue-400">
+              Home
+            </NavLink>
+          </div>
+          <div className="max-w-4xl px-4 mx-auto mt-5">{children}</div>
+        </main>
+      </Provider>
     </>
   )
 }
