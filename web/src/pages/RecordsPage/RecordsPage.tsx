75ea4d9e1d094cd663a4878890638cd5b2f5e933
Iverson Diles <ivcotad@gmail.com>
2022-07-23 09:12:59 -0700

    sorting & context

--- web/src/pages/RecordsPage/RecordsPage.tsx
+++ web/src/pages/RecordsPage/RecordsPage.tsx
@@ -1,15 +1,37 @@
-import { Link, routes } from '@redwoodjs/router'
+import { useForm } from '@redwoodjs/forms'
 import { MetaTags } from '@redwoodjs/web'
+import { createContext } from 'react'
 import RecordsCell from 'src/components/RecordsCell/'
 
-const RecordsPage = () => {
-  return (
-    <>
-      <MetaTags title="Records" description="Records page" />
+interface RPCI {
+  territoryName: string
+}
 
+export const RecordsPageContext = createContext<RPCI>(null)
+
+interface IForm {
+  territoryName: string
+}
+
+const RecordsPage = () => {
+  const { register, watch } = useForm<IForm>()
+
+  return (
+    <RecordsPageContext.Provider
+      value={{
+        territoryName: watch('territoryName'),
+      }}
+    >
+      <MetaTags title="Records" description="Records page" />
       <h1 className="text-2xl font-extrabold">Records Page</h1>
+      <input
+        placeholder="Territory Name"
+        {...register('territoryName')}
+        type="text"
+      />
+
       <RecordsCell />
-    </>
+    </RecordsPageContext.Provider>
   )
 }
 
