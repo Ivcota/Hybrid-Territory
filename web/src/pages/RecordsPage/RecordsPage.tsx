a426d76a53bff34569b39d0a2cb07a7429df1fa3
Iverson Diles <ivcotad@gmail.com>
2022-07-23 18:14:30 -0700

    polish

--- web/src/pages/RecordsPage/RecordsPage.tsx
+++ web/src/pages/RecordsPage/RecordsPage.tsx
@@ -1,6 +1,6 @@
 import { Controller, useForm } from '@redwoodjs/forms'
 import { MetaTags } from '@redwoodjs/web'
-import { createContext, useEffect, useMemo } from 'react'
+import { useEffect, useMemo } from 'react'
 import ReactSelect from 'react-select'
 import RecordsCell from 'src/components/RecordsCell/'
 import { useAllTerritoryNamesQuery } from 'src/generated/graphql'
@@ -14,17 +14,18 @@ interface IForm {
 }
 
 const RecordsPage = () => {
-  /**
-   * TODO
-   * Build a form connection and create a filter on the Cell.
-   */
-
-  const { register, watch, control, reset } = useForm<IForm>()
+  const { watch, control, reset } = useForm<IForm>()
   const { data, loading } = useAllTerritoryNamesQuery()
 
   const generateOptions = useMemo(() => {
     if (!loading && data) {
-      const newArray = data.territories.map((item) => {
+      const sortedArray = data.territories.slice().sort((a, b) =>
+        a.name.localeCompare(b.name, undefined, {
+          numeric: true,
+        })
+      )
+
+      const newArray = sortedArray.map((item) => {
         return {
           label: item.name,
           value: item.id,
@@ -34,10 +35,6 @@ const RecordsPage = () => {
       return newArray
     }
   }, [data, loading])
-
-  useEffect(() => {
-    console.log(watch('territoryName')?.value)
-  }, [watch('territoryName')])
 
   return (
     <>
@@ -53,6 +50,7 @@ const RecordsPage = () => {
           render={({ field }) => (
             <ReactSelect
               onChange={field.onChange}
+              value={field.value}
               options={generateOptions}
               isSearchable
               className="mt-3"
@@ -65,7 +63,7 @@ const RecordsPage = () => {
           onClick={() => {
             reset()
           }}
-          className="px-3 py-1 mt-3 text-white bg-red-500 rounded-sm hover:bg-red-600 active:bg-red-700 "
+          className="px-3 py-1 mt-3 text-white transition-all duration-200 bg-red-500 rounded-sm  hover:bg-red-600 active:bg-red-700"
         >
           Clear
         </button>
