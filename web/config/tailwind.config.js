9b8bfc19ab8b0889412e5611d5693a9a822adf62
Logan Ricard <logvnjs@gmail.com>
2022-07-26 13:35:43 -0500

    added styling for Login Inputs

--- web/config/tailwind.config.js
+++ web/config/tailwind.config.js
@@ -1,7 +1,22 @@
 module.exports = {
   content: ['src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    fontFamily: {
+      Albert: ['Albert Sans', 'sans-serif'],
+      Roboto: ['Roboto Slab', 'serif'],
+      OpenSans: ['Open Sans', 'sans-serif'],
+    },
+    extend: {
+      colors: {
+        'off-white': "#F6FFF8",
+        'teal-blue': "#74CBE7",
+        'light-blue': "#1556AC",
+        'dark-blue': "#184379",
+        'accent': "#FC684A",
+        'off-black': "#000505",
+        'error': "#c1121f",
+      },
+    },
   },
   plugins: [],
 }
