3ac8c1bae1e8d4cd16f6cf597ff9f80f7c58da39
Logan Ricard <logvnjs@gmail.com>
2022-08-02 14:25:20 -0500

    extracted Territory Card Component and Added Custom HTD Button

--- web/src/components/AvailableTerritoriesCell/AvailableTerritoriesCell.tsx
+++ web/src/components/AvailableTerritoriesCell/AvailableTerritoriesCell.tsx
@@ -10,6 +10,8 @@ import {
 } from 'src/generated/graphql'
 import type { AvailableTerritoriesQuery } from 'types/graphql'
 
+import Button from '../Button/Button'
+
 export const QUERY = gql`
   query AvailableTerritoriesQuery {
     availableTerritories {
@@ -37,16 +39,6 @@ export const Failure = ({ error }: CellFailureProps) => (
 export const Success = ({
   availableTerritories,
 }: CellSuccessProps<AvailableTerritoriesQuery>) => {
-  const [updateTerritory] = useUpdateTerritoryMutation({
-    refetchQueries: ['AvailableTerritoriesQuery'],
-  })
-
-  const { currentUser } = useAuth()
-  const [sendMessage] = useSendMessageMutation()
-  const [createRecord] = useCreateRecordMutation()
-
-  const now = dayjs()
-
   return (
     <>
       <div className="flex flex-col flex-wrap items-center justify-center gap-4 md:flex-row">
@@ -57,55 +49,67 @@ export const Success = ({
           )
           .map((item) => {
             return (
-              <div
-                className="flex flex-col items-center gap-3 px-10 py-4 mt-3 transition-all duration-300 rounded-lg shadow-lg hover:-translate-y-1"
-                key={item.id}
-              >
-                <h2 className="text-2xl text-center"> {item.name} </h2>
-                <button
-                  className="px-3 py-2 text-white transition-all duration-200 rounded-sm shadow-md bg-gradient-to-br from-green-500 to-green-600 hover:from-indigo-500 hover:to-indigo-600 active:from-indigo-600 active:to-indigo-670"
-                  onClick={async () => {
-                    await toast.promise(
-                      updateTerritory({
-                        variables: {
-                          id: item.id,
-                          input: {
-                            userId: currentUser?.id,
-                          },
-                        },
-                      }),
-                      {
-                        loading: 'loading...',
-                        error: 'Error...',
-                        success: `${item.name} has been assigned to you.`,
-                      }
-                    )
-
-                    await createRecord({
-                      variables: {
-                        input: {
-                          territoryId: item.id,
-                          userId: currentUser.id,
-                          checkoutDate: now,
-                        },
-                      },
-                    })
-
-                    await sendMessage({
-                      variables: {
-                        phone: process.env.REDWOOD_ENV_PHONENUMBER,
-                        message: `${currentUser?.firstName} checked out territory card ${item.name} at ${now}.`,
-                      },
-                    })
-                  }}
-                >
-                  Checkout
-                </button>
-              </div>
+              <TerritoryCard item={item} />
             )
           })}
       </div>
       <Toaster />
     </>
+  )
+}
+
+const TerritoryCard = ({ item }) => {
+  const [updateTerritory] = useUpdateTerritoryMutation({
+    refetchQueries: ['AvailableTerritoriesQuery'],
+  })
+  const { currentUser } = useAuth()
+  const [sendMessage] = useSendMessageMutation()
+  const [createRecord] = useCreateRecordMutation()
+
+  const now = dayjs()
+  return (
+    <div
+      className="flex flex-row justify-between items-center gap-3 px-4 py-4 mt-3 transition-all duration-300 rounded-lg shadow-lg hover:-translate-y-1 min-w-[88%]"
+      key={item.id}
+    >
+      <h2 className="text-xl text-center font-Roboto font-medium tracking-wider text-off-black"> {item.name} </h2>
+      <Button
+        variant='outline'
+        onClick={async () => {
+          await toast.promise(
+            updateTerritory({
+              variables: {
+                id: item.id,
+                input: {
+                  userId: currentUser?.id,
+                },
+              },
+            }),
+            {
+              loading: 'loading...',
+              error: 'Error...',
+              success: `${item.name} has been assigned to you.`,
+            }
+          )
+          await createRecord({
+            variables: {
+              input: {
+                territoryId: item.id,
+                userId: currentUser.id,
+                checkoutDate: now,
+              },
+            },
+          })
+          await sendMessage({
+            variables: {
+              phone: process.env.REDWOOD_ENV_PHONENUMBER,
+              message: `${currentUser?.firstName} checked out territory card ${item.name} at ${now}.`,
+            },
+          })
+        }}
+      >
+        Checkout
+      </Button>
+    </div>
   )
 }
--- web/src/pages/SelfCheckoutPage/SelfCheckoutPage.tsx
+++ web/src/pages/SelfCheckoutPage/SelfCheckoutPage.tsx
@@ -6,8 +6,8 @@ const SelfCheckoutPage = () => {
     <>
       <MetaTags title="Self-Checkout" description="Self-Checkout page" />
 
-      <h1 className="text-2xl font-extrabold ">Self Checkout</h1>
-      <p className="mt-2">
+      <h1 className="text-2xl font-bold font-Roboto text-dark-blue">Self Checkout</h1>
+      <p className="mt-2 font-OpenSans italic font-light text-center text-off-black/80 text-sm tracking-wide">
         You can checkout territory yourself using this page. Simply select the
         territory you’d like to use, and it will automatically be added to your
         territories page.
