0979576d1baaa8e7cdf117832d69edbf3b408d82
Logan Ricard <logvnjs@gmail.com>
2022-08-29 13:31:32 -0500

    added ThemeToggle component

---
+++ web/src/components/ThemeToggle/ThemeToggle.stories.tsx
@@ -0,0 +1,7 @@
+import ThemeToggle from './ThemeToggle'
+
+export const generated = () => {
+  return <ThemeToggle />
+}
+
+export default { title: 'Components/ThemeToggle' }
---
+++ web/src/components/ThemeToggle/ThemeToggle.tsx
@@ -0,0 +1,10 @@
+const ThemeToggle = () => {
+  return (
+    <div>
+      <h2>{'ThemeToggle'}</h2>
+      <p>{'Find me in ./web/src/components/ThemeToggle/ThemeToggle.tsx'}</p>
+    </div>
+  )
+}
+
+export default ThemeToggle
