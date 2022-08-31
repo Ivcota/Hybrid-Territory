dfad49ece39aeb8ac3c82860fafce54c6d423d96
Logan Ricard <logvnjs@gmail.com>
2022-08-31 16:31:21 -0500

    fixed bug with Available Photo Icon clipping through NavBar

--- web/src/components/Navbar/Navbar.tsx
+++ web/src/components/Navbar/Navbar.tsx
@@ -17,7 +17,7 @@ const Navbar = () => {
   const { isActive, toggle, currentUser } = useContext(layoutContext)
 
   return (
-    <nav className="sticky top-0 py-4 bg-white shadow-lg ">
+    <nav className="sticky top-0 z-10 py-4 bg-white shadow-lg">
       <div className="px-4 mx-auto lg:px-8">
         <div className="flex items-center justify-between gap-5 ">
           <Logo />
