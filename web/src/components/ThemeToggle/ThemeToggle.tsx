42aba5f3a2f8a97737b6d7a3de51db64231e12c1
Logan Ricard <logvnjs@gmail.com>
2022-10-05 11:45:18 -0500

    updated Theme Switching btn to use Mantine ActionIcon

--- web/src/components/ThemeToggle/ThemeToggle.tsx
+++ web/src/components/ThemeToggle/ThemeToggle.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 import { BsSun, BsMoon } from 'react-icons/bs'
+import { ActionIcon } from '@mantine/core'
 
 import { useThemeToggle } from 'src/hooks/useTheme'
 
@@ -12,7 +13,9 @@ const ThemeToggle = () => {
       className="flex items-center justify-center w-8 h-7"
       onClick={themeToggleHandler}
     >
-      {theme === 'light' ? <BsSun size={20} /> : <BsMoon size={20} />}
+      <ActionIcon variant="subtle" className='hover:text-light-blue dark:hover:text-sky-blue-dark hover:bg-off-white dark:hover:bg-off-black/80'>
+        {theme === 'light' ? <BsSun size={18} /> : <BsMoon size={18} />}
+      </ActionIcon>
     </div>
   )
 }
