1b3e86f55fe804adfba8735681b23013ca635fcc
Iverson Diles <ivcotad@gmail.com>
2022-12-08 19:23:24 -0700

    about page fix

--- web/src/pages/AboutPage/AboutPage.tsx
+++ web/src/pages/AboutPage/AboutPage.tsx
@@ -5,11 +5,13 @@ const AboutPage = () => {
     <>
       <MetaTags title="About" description="About page" />
 
-      <h1 className="font-serif text-3xl dark:text-off-white-dark">About</h1>
-      <p className="mt-2 dark:text-off-white-dark">
-        A simple territory distribution app. Documentation on how to use this
-        will be provied shortly.
-      </p>
+      <div className="px-6 mx-auto max-w-7xl">
+        <h1 className="font-serif text-3xl dark:text-off-white-dark">About</h1>
+        <p className="mt-2 dark:text-off-white-dark">
+          A simple territory distribution app. Documentation on how to use this
+          will be provied shortly.
+        </p>
+      </div>
     </>
   )
 }
